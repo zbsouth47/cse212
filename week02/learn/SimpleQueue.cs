5bc27b43776f09a9e804d3a058c90d3973d16b4d
zbsouth47 <zbsouth47@gmail.com>
2024-05-02 21:23:21 -0600

    fixed learn

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -11,6 +11,7 @@
         var value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found:
+        // index values in dequeue function were incorrect
 
         Console.WriteLine("------------");
 
@@ -29,6 +30,7 @@
         value = queue.Dequeue();
         Console.WriteLine(value);
         // Defect(s) Found: 
+        // enqueue function inserted new values at the beginning of the queue, not the end
 
         Console.WriteLine("------------");
 
@@ -54,7 +56,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +68,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
     }
 }
